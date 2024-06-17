9f3795727f9af4480ca9c78306d919196a12f94a
Anton <allians19@gmail.com>
2024-06-17 09:56:37 +0500

    Реализация функций по ДЗ Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+    this.name = name,
+    this.gender= gender,
+    this.age = age,
+    this.marks = []
 }
 
+
 Student.prototype.setSubject = function (subjectName) {
-  
+   return this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(this.hasOwnProperty('marks')) {
+        this.marks.push(...marks)
+    }
+    
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(this.marks && this.marks.length > 0) {
+        return this.marks.reduce((acc, item) => acc + item) / this.marks.length
+    } else {
+        return 0
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
