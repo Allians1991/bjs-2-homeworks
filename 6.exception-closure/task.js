862d039a01463a6e6bb79fb3023e31a3ed9f4005
Anton <allians19@gmail.com>
2024-07-14 14:17:28 +0500

    Домашнее задание к лекции 6. «Обработка исключений и замыкания»

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿function parseCount(val) {
+    if (isNaN(val)) {
+        throw new Error('Невалидное значение');
+    }
+    return Number.parseFloat(val);
+}
+
+function validateCount(val) {
+    try {
+        return parseCount(val);
+    } catch(error) {
+        return error;
+    }
+}
+
+
+class Triangle {
+    constructor(a, b, c) {
+        
+        if(a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+        
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c)
+    } catch {
+        return {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует'
+            },
+            get area() {
+                return 'Ошибка! Треугольник не существует'
+            }
+        }
+    }
+}
